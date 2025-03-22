e01c0d4c5333c970130a000e17ecc0fb79c21a83
vamsi Krisna Nallapuneni <76245813+NALLAPUNENIVAMSIKRISHNA@users.noreply.github.com>
2025-03-22 11:09:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
 
+## code
+####
+
 # Arxiv and Wikipedia tools
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=250)
 wiki = WikipediaQueryRun(api_wrapper=api_wrapper)
